d71f3454978cae26b7f9bb6b397a9d1ab1521acf
Petrinec <Jakub.Petrinec@t-systems.com>
2021-11-11 09:33:29 +0100

    zaciatocny subor hry

---
+++ game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+
+    print('''
+===============================================================================================
+                                  Vitaj v hre ESCAPE ROOM.                                    
+Nachadzas sa v tmavej miestnosti. Nic momentalne nevidis. Musis sa odtialto dostat lebo umries!
+===============================================================================================
+    ''')
