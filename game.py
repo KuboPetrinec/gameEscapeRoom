910036e91ad59182c0a334c725ea92cc70c1693d
A106928093 <jakub.petrinec@t-systems.com>
2021-11-11 13:21:33 +0100

    vytvorenie dictionary room + funkcia show_room(room)

--- game.py
+++ game.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 import states
 
+#vytvaranie dictionary ktory bude vytvarat miestnosti
+room = {
+    'name' : 'dungeon',
+    'description' : 'Nachadzas sa v tmavej zatuchnutej miestnosti bez okien, co dava tusit, ze si niekolko metrov'
+        'pod zemou. Zeby kosicky hrad? Aj to je mozne, ti prebleslo hlavou.',
+    'items' : [],
+    'exits' : []
+}
+
 # na pokracovanie hrania pouzivame premenne nie ich hodnoty
 STATE_PLAYING = 'playing'
 STATE_QUIT = 'quit'
+
 
 # vypise zakladne info o hre
 def about_game():
@@ -11,7 +21,7 @@ def about_game():
     print('(c)2021 Hra by JAKUB')
 
 
-commands = ['o hre', 'prikazy', 'koniec']
+commands = ['o hre', 'prikazy', 'koniec', 'rozhliadni sa', 'alliasy']
 
 
 # vypis vsetkych prikazov
@@ -19,10 +29,21 @@ def prikazy():
     print("Zoznam dostupnych prikazov :")
     print(commands)
 
+def rozhliadni_sa():
+    print('Nachadzas sa v tmavej zatuchnutej miestnosti bez okien, co dava tusit, ze si niekolko metrov pod zemou. '
+          'Zeby kosicky hrad? Aj to je mozne, ti prebleslo hlavou.')
+
+def show_allias():
+    print("Alliasy pre prikazy:")
+    print("koniec=quit/q/bye, o hre=about/info, prikazy=commands/help/?")
+
+where = None
+
+def show_room(room):
+    print(f"Nachadzas sa v miesnosti {room['name']} , {room['description']}")
 
 # varianty prikazu na ukoncenie hry
 the_end = ('', 'koniec', 'quit', 'bye', 'q')
-
 
 if __name__ == '__main__':
 
@@ -32,15 +53,18 @@ if __name__ == '__main__':
 Nachadzas sa v tmavej miestnosti. Nic momentalne nevidis. Musis sa odtialto dostat lebo umries!
 ===============================================================================================
     ''')
-    #rendering the dark room (starting room)
-    print('Nachadzas sa v tmavej zatuchnutej miestnosti bez okien, co dava tusit, ze si niekolko metrov pod zemou. Zeby kosicky hrad? Aj to je mozne, ti prebleslo hlavou.')
+    # rendering the dark room (starting room)
+    show_room(room)
+    #rozhliadni_sa()
+    #print('Nachadzas sa v tmavej zatuchnutej miestnosti bez okien, co dava tusit, ze si niekolko metrov pod zemou. '
+          #'Zeby kosicky hrad? Aj to je mozne, ti prebleslo hlavou.')
 
     # main loop
     game_state = states.STATE_PLAYING
     line = None
 
     while game_state == states.STATE_PLAYING:
-        #normalizing string
+        # normalizing string
         line = input('> ').lower().strip()
 
         if line == '':
@@ -52,12 +76,20 @@ Nachadzas sa v tmavej miestnosti. Nic momentalne nevidis. Musis sa odtialto dost
         elif line in ('o hre', 'about', 'info'):
             about_game()
 
+        elif line in ('rozhliadni sa','look around'):
+            rozhliadni_sa()
+            show_room(room)
+
+        elif line in ('alias'):
+            show_allias()
+
         elif line in ('prikazy', 'commands', 'help', '?'):
             # prikazy()
             print("Zoznam prikazov v hre:")
             print("* koniec - ukonci rozohratu hru")
             print("* o hre - vypise informacie o hre")
             print("# prikazy - vypise vsetky dostupne prikazy")
-            print("* alliasy : koniec=quit/q/bye, o hre=about/info, prikazy=commands/help/?")
+            print("* rozhliadni sa - vypise popis miestnosti")
+            print("* alliasy : koniec=quit/q/bye, o hre=about/info, prikazy=commands/help/?, rozhliadni sa=look around")
         else:
             print("Taky prikaz nepoznam")
