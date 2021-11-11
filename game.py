4a3b9a47478812436067696d6248f8e251fae05e
A106928093 <jakub.petrinec@t-systems.com>
2021-11-11 11:28:52 +0100

    commit z pycharmu

--- game.py
+++ game.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
+import states
+
+
+# na pokracovanie hrania pouzivame premenne nie ich hodnoty
+STATE_PLAYING = 'playing'
+STATE_QUIT = 'quit'
+
+# vypise zakladne info o hre
+def about_game():
+    print("Ocitol si sa v hre Indiana Jones vytvorenej Kubom.")
+    print('(c)2021 Hra by JAKUB')
+
+
+commands = ['o hre', 'prikazy', 'koniec']
+
+
+# vypis vsetkych prikazov
+def prikazy():
+    print("Zoznam dostupnych prikazov :")
+    print(commands)
+
+
+# varianty prikazu na ukoncenie hry
+the_end = ('', 'koniec', 'quit', 'bye', 'q')
+
 
 if __name__ == '__main__':
 
@@ -10,10 +35,28 @@ Nachadzas sa v tmavej miestnosti. Nic momentalne nevidis. Musis sa odtialto dost
     ''')
 
     # main loop
+    game_state = states.STATE_PLAYING
     line = None
 
-    while str(line.lower()) != 'koniec':
-        line = input('< ')
+    while game_state == states.STATE_PLAYING:
+        #normalizing string
+        line = input('> ').lower().strip()
 
+        if line == '':
+            continue
 
-    print('(c)2021 Hra by JAKUB')
+        elif line in ('', 'koniec', 'quit', 'bye', 'q'):
+            game_state = states.STATE_QUIT
+
+        elif line in ('o hre', 'about', 'info'):
+            about_game()
+
+        elif line in ('prikazy', 'commands', 'help', '?'):
+            # prikazy()
+            print("Zoznam prikazov v hre:")
+            print("* koniec - ukonci rozohratu hru")
+            print("* o hre - vypise informacie o hre")
+            print("# prikazy - vypise vsetky dostupne prikazy")
+            print("* alliasy : koniec=quit/q/bye, o hre=about/info, prikazy=commands/help/?")
+        else:
+            print("Taky prikaz nepoznam")
