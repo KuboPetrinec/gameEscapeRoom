9fe362db1ebf31b18307eb85c80f4ad323f0a8e4
Petrinec <jakub.petrinec@t-systems.com>
2021-11-11 09:42:12 +0100

    prvy while

--- game.py
+++ game.py
@@ -4,7 +4,16 @@ if __name__ == '__main__':
 
     print('''
 ===============================================================================================
-                                  Vitaj v hre ESCAPE ROOM.                                    
+                          Vitaj v hre INDIAN JONES :  ESCAPE ROOM.                                    
 Nachadzas sa v tmavej miestnosti. Nic momentalne nevidis. Musis sa odtialto dostat lebo umries!
 ===============================================================================================
     ''')
+
+    # main loop
+    line = None
+
+    while str(line.lower()) != 'koniec':
+        line = input('< ')
+
+
+    print('(c)2021 Hra by JAKUB')
