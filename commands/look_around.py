a42a68db471a37718b856295b85a017e442483c2
A106928093 <jakub.petrinec@t-systems.com>
2021-11-18 11:25:55 +0100

    zabudol som dat add

---
+++ commands/look_around.py
@@ -0,0 +1,9 @@
+def _exec(context: dict, param: str):
+    show_room(context['room'])
+
+cmd = {
+    'name': "rozhliadni sa",
+    'description': 'vypíše opis miestnosti, v ktorej sa hráč práve nachádza',
+    'aliases': ("look around", "kukaj het"),
+    'exec': _exec,
+}
---
+++ commands/quit.py
@@ -0,0 +1,9 @@
+def _exec(context: dict, param: str):
+    context['state'] = states.QUIT
+
+cmd =    {
+        'name': 'koniec',
+        'description': 'ukončí rozohratú hru',
+        'aliases': ('quit', 'bye', 'q', 'exit',),
+        'exec': _exec,
+}
